c325e0a574a7b1078ea337bd7b955cabdc076179
Ankur Pande <ankur.pande108@gmail.com>
2021-10-05 11:35:32 +0530

    Initial Commit

---
+++ src/com/cms/entity/City.java
@@ -0,0 +1,35 @@
+package com.cms.entity;
+
+/**
+ * city is entity class
+ * created by @Ankur Pande
+ */
+public class City {
+
+    private Integer cityId;
+    private String cityName;
+
+    public Integer getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Integer cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "cityId=" + cityId +
+                ", cityName='" + cityName + '\'' +
+                '}';
+    }
+}
