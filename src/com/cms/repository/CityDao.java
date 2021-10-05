9359af39194e022521abd587a46e5792482d013f
Ankur Pande <ankur.pande108@gmail.com>
2021-10-05 11:39:53 +0530

    Initial Commit

---
+++ src/com/cms/repository/CityDao.java
@@ -0,0 +1,15 @@
+package com.cms.repository;
+
+import com.cms.entity.City;
+
+/**
+ * CityDao is interface class
+ *
+ * created by @Ankur Pande
+ *
+ */
+
+public interface CityDao {
+
+    void addCity(City city);
+}
